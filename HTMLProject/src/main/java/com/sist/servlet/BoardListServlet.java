efebe52cab0a1f6b1f6dd3bc837a39a179a79f96
yoo-hyunseung <hsung122@naver.com>
2023-05-25 14:12:13 +0900

    table.css

--- HTMLProject/src/main/java/com/sist/servlet/BoardListServlet.java
+++ HTMLProject/src/main/java/com/sist/servlet/BoardListServlet.java
@@ -22,7 +22,7 @@ import com.sist.dao.*;
 @WebServlet("/BoardListServlet")
 public class BoardListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// JSP
 		// 1. 변환 => 전송 (HTML,XML,JSON)
@@ -30,14 +30,30 @@ public class BoardListServlet extends HttpServlet {
 		response.setContentType("text/html;charset=UTF-8");
 		// XML => text/xml , JSON => text/plain 
 		PrintWriter out=response.getWriter();
+		// 사용자가 요청한 페이지를 받는다. => request
+		String strPage = request.getParameter("page");
+//		if(strPage==null) {
+//			strPage="1";
+//		}
+		int curpage = Integer.parseInt(strPage);
 		// 사용자의 브라우저에서 읽어가는 위치를 설정 => OutputStream 
 		BoardDAO dao=BoardDAO.newInstance();
 		List<BoardVO> list=dao.boardListData(1);
 		out.println("<html>");
+		out.println("<head>");
+		out.println("<link rel=stylesheet href=table.css>");
+		out.println("</head>");
 		out.println("<body>");
+//		out.println("</body>");
 		out.println("<center>");
 		out.println("<h1>자유 게시판</h1>");
-		out.println("<table width=700 border=1 bordercolor=black>");
+		out.println("<table width=700 class=table_content>");
+		out.println("<tr>");
+		out.println("<td>");
+		out.println("<a href=BoardInsertServlet>새글</a>");
+		out.println("</td>");
+		out.println("</tr>");
+		out.println("<table width=700 class=table_content>");
 		out.println("<tr>");
 		out.println("<th width=10%>번호</th>");
 		out.println("<th width=45%>제목</th>");
@@ -47,7 +63,7 @@ public class BoardListServlet extends HttpServlet {
 		out.println("</tr>");
 		for(BoardVO vo:list)
 		{
-			out.println("<tr>");
+			out.println("<tr class=dataTr>");
 			out.println("<td width=10% align=center>"+vo.getNo()+"</td>");
 			out.println("<td width=45%>"+vo.getSubject()+"</td>");
 			out.println("<td width=15% align=center>"+vo.getName()+"</td>");
@@ -55,7 +71,15 @@ public class BoardListServlet extends HttpServlet {
 			out.println("<td width=10% align=center>"+vo.getHit()+"</td>");
 			out.println("</tr>");
 		}
+		out.println("<tr>");
+		out.println("<td colspan=5 align=center>");
+		out.println("<a href=#>이전</a>");
+		out.println("0 page / 0pages");
+		out.println("<a href=#>다음</a>");
+		out.println("</td>");
+		out.println("</tr>");
 		out.println("</table>");
+		
 		out.println("</center>");
 		out.println("</body>");
 		out.println("</html>");
